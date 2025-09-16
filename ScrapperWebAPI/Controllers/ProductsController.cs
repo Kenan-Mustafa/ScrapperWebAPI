4b7663ad420ed54e8404f40b7c92e8c407ab06a7
Kenan Mustafa <kenan.mustafa.2001@mail.ru>
2025-09-16 16:11:29 +0400

    v1

--- ScrapperWebAPI/Controllers/ProductsController.cs
+++ ScrapperWebAPI/Controllers/ProductsController.cs
@@ -1,54 +1,63 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ScrapperWebAPI.Helpers;
 using ScrapperWebAPI.Helpers.Product;
-using ScrapperWebAPI.Models.GoSport;
 using ScrapperWebAPI.Models.ProductDtos;
 
-namespace ScrapperWebAPI.Controllers
+namespace ScrapperWebAPI.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    [HttpGet]
+    public async Task<IActionResult> Get(string store, string category)
     {
-        [HttpGet]
-        public async Task<IActionResult> Get(string store,string category)
+        if (store.ToLower() == "gosport")
         {
-            var a = await GetZaraProduct.GetByCategoryName(category);
-            return Ok(a);
-            if (store.ToLower() == "gosport")
+            var data = await GetGoSportProducts.GetByProductByBrand(category);
+            return Ok(data);
+        }
+        else if (store.ToLower() == "zara")
+        {
+            var data = await GetZaraProduct.GetByCategoryName(category);
+            return Ok(data);
+        }
+        return BadRequest("This store can not be found");
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAll(string store)
+    {
+        var list = new HashSet<ProductToListDto>();
+
+        if (store.ToLower() == "zara")
+        {
+            var categories = await GetZaraCategories.GetAll();
+
+            foreach (var category in categories)
             {
-                var data = await GetGoSportProducts.GetByProductByBrand(category);
-                return Ok(data);
+                var products = await GetZaraProduct.GetByCategoryName(category.Name);
+                foreach (var product in products)
+                {
+                    list.Add(product);
+                }
             }
-            else if (store.ToLower() == "zara")
+        }
+        if (store.ToLower() == "gosport")
+        {
+            var brands = await GetGoSportBrands.GetAll();
+
+            foreach (var brand in brands)
             {
-                var data = await GetZaraProduct.GetByCategoryName(category);
-                return Ok(data);
+                var products = await GetGoSportProducts.GetByProductByBrand(brand.Name);
+                foreach (var product in products)
+                {
+                    list.Add(product);
+                }
             }
-            return Ok("Hello from ProductsController");
         }
 
-        [HttpGet("all")]
-        public async Task<IActionResult> GetAll(string store)
-        {
-            //var list = new HashSet<ProductToListDto>();
-            //var categories = await GetZaraCategories.GetAll();
+        return Ok(list);
 
-            //foreach (var category in  categories)
-            //{
-            //    var products = await GetZaraProduct.GetByCategoryName(category.Name);
-            //    foreach (var product in products)
-            //    {
-            //        list.Add(product);
-            //    }
-            //}   
-
-
-            var products = await GetZaraProduct.GetByCategoryName("MAN-ПРОСМОТРЕТЬ ВСЕ");
-            return Ok(products.Count());
-            //return Ok(list.Count());
-
-        }
     }
 }
